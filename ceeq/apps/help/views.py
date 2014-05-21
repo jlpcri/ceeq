6b8aea5b439f311c58302f3a6f7577db8a2c9746
Songqing Liu <sliu@west.com>
2014-05-21 11:15:27 -0500

    Update guide

--- ceeq/apps/help/views.py
+++ ceeq/apps/help/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 
 
 def help_guide(request):
-    return render(request, 'guide.html')
+    context = {
+        'base_url': 'http://' + request.get_host()
+    }
+    return render(request, 'guide.html', context)
 
 
 def help_faq(request):
