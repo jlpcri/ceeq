20c5c426e5dcccf8421e38b50880631318053721
Songqing Liu <sliu@west.com>
2014-06-04 15:43:13 -0500

    defects density history record deletion

--- ceeq/apps/defects_density/views.py
+++ ceeq/apps/defects_density/views.py
@@ -1,8 +1,10 @@
 from django.contrib import messages
+from django.contrib.auth.decorators import user_passes_test
 from django.core.urlresolvers import reverse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.template import RequestContext
 from ceeq.apps.projects.models import ProjectComponentsDefectsDensity
+from ceeq.apps.users.views import user_is_superuser
 
 
 def dd_all(request):
@@ -26,9 +28,10 @@ def dd_new(request):
     pass
 
 
+@user_passes_test(user_is_superuser)
 def dd_delete(request, dd_id):
     dd = get_object_or_404(ProjectComponentsDefectsDensity, pk=dd_id)
     dd.delete()
-    messages.success(request, "DD record \"{0}\" has been deleted.".format(dd.id))
+    messages.success(request, "Defects Density project: \"{0}\", version: \"{1}\", data: \"{2}\" has been deleted.".format(dd.project.name, dd.version, dd.created))
 
-    return redirect(reverse('ceeq.apps.defects_density.views.dd_all'))
+    return redirect(reverse('ceeq.apps.users.views.home'))
