c106747baceb7f040b773380a036329ca18df41b
Songqing Liu <sliu@west.com>
2014-06-04 10:33:27 -0500

    management commands for daily schedule job

--- ceeq/apps/projects/management/commands/daily_dd_log.py
+++ ceeq/apps/projects/management/commands/daily_dd_log.py
@@ -1,6 +1,8 @@
 from django.core.management.base import BaseCommand, CommandError
 from ceeq.apps.projects.models import Project, ProjectComponentsDefectsDensity
 
+from ceeq.apps.projects.views import defects_density_single_log
+
 
 class Command(BaseCommand):
     args = '<project_id project_id ...>'
@@ -13,11 +15,15 @@ class Command(BaseCommand):
                     project = Project.objects.get(pk=project_id)
                 except Project.DoesNotExist:
                     raise CommandError('Project "%s" does not exist' % project_id)
-                print project.name
-                self.stdout.write('Successfully saved DD for project "%s"' % project_id)
+                defects_density_single_log(None, project)
+                self.stdout.write('Successfully saved Defects Density for project "%s"' % project_id)
         else:
             projects = Project.objects.all()
             for project in projects:
-                print project.name
-            self.stdout.write("All projects DD saved")
+                if project.score < 0:
+                    continue
+                else:
+                    defects_density_single_log(None, project)
+
+            self.stdout.write("All projects Defects Density saved")
 
