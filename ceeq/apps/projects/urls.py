45107a798d1d69bfd738ccbe7efd7f1f22e6b1f2
Songqing Liu <sliu@west.com>
2014-08-28 11:10:28 -0500

    Separate sub components pie chart handle

--- ceeq/apps/projects/urls.py
+++ ceeq/apps/projects/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, url, include
 
 
 urlpatterns = patterns('ceeq.apps.projects.views',
@@ -16,14 +16,5 @@ urlpatterns = patterns('ceeq.apps.projects.views',
       url(r'^project/dd_pie/(?P<project_id>\d+)/$', 'fetch_defects_density_score_pie', name='fetch_defects_density_score_pie'),
       #url(r'^characteristics/$', 'characteristics', name='characteristics'),
 
-      url(r'^project/(?P<project_id>\d+)/Application/$', 'project_sub_apps_piechart', name='project_sub_apps_piechart'),
-      url(r'^project/(?P<project_id>\d+)/CXP/$', 'project_sub_cxp_piechart', name='project_sub_cxp_piechart'),
-      url(r'^project/(?P<project_id>\d+)/Platform/$', 'project_sub_platform_piechart', name='project_sub_platform_piechart'),
-      url(r'^project/(?P<project_id>\d+)/Reports/$', 'project_sub_reports_piechart', name='project_sub_reports_piechart'),
-
-      url(r'^project/apps_sub_pie/(?P<project_id>\d+)/$', 'fetch_apps_subcomponents_pie', name='fetch_apps_subcomponents_pie'),
-      url(r'^project/cxp_sub_pie/(?P<project_id>\d+)/$', 'fetch_cxp_subcomponents_pie', name='fetch_cxp_subcomponents_pie'),
-      url(r'^project/platform_sub_pie/(?P<project_id>\d+)/$', 'fetch_platform_subcomponents_pie', name='fetch_platform_subcomponents_pie'),
-      url(r'^project/reports_sub_pie/(?P<project_id>\d+)/$', 'fetch_reports_subcomponents_pie', name='fetch_reports_subcomponents_pie'),
-
+      url(r'^', include('ceeq.apps.projects.urls_sub_components')),
       )
