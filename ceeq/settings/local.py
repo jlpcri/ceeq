ec2905658102a89ee60cd5e456c436a2e0d3bc01
Songqing Liu <sliu@west.com>
2014-09-15 10:48:52 -0500

    static files configuration for laptop

--- ceeq/settings/local.py
+++ ceeq/settings/local.py
@@ -9,5 +9,6 @@ ALLOWED_HOSTS = ['*']
 if socket.gethostname() == 'sliu-OptiPlex-GX520':  # desktop
     #STATIC_URL = 'http://apps.qaci01.wic.west.com/static/'
     STATICFILES_DIRS = ('/opt/static/',)
-elif socket.gethostname() == 'OM1960l1':
-    STATICFILES_DIRS = ('',)
+elif socket.gethostname() == 'OM1960L1':
+    STATIC_ROOT = '/static/'
+    STATICFILES_DIRS = ('c:/static/',)
