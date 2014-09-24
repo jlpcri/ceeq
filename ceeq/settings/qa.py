122432e7754968ccccec8a2d4a884bbe6726f865
Songqing Liu <sliu@west.com>
2014-09-24 12:14:20 -0500

    Apply PostgresSQL on staging server

--- ceeq/settings/qa.py
+++ ceeq/settings/qa.py
@@ -12,6 +12,19 @@ DEBUG = False
 ALLOWED_HOSTS = [
     'apps.qaci01.wic.west.com',
     'apps.qaci01',
+    'linux6436.wic.west.com',
+    'linux6436',
     'linux6438.wic.west.com',
     'linux6438'
 ]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ceeq',
+        'USER': 'visilog',
+        'PASSWORD': '6ewuON0>;wHTe(DttOwjg#5NY)U497xKVwOxmQt60A1%}r:@qC&`7OdSP8u[.l[',
+        'HOST': 'linux6437.wic.west.com',
+        'PORT': '5432'
+    }
+}
