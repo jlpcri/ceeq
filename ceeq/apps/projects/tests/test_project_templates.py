f96e24c00c0e1038750e3672acc6ab2dd36b4008
Songqing Liu <sliu@west.com>
2014-10-13 10:42:56 -0500

    more unit tests

--- ceeq/apps/projects/tests/test_project_templates.py
+++ ceeq/apps/projects/tests/test_project_templates.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.test import Client, TestCase
 from django.core.urlresolvers import resolve, reverse
 
@@ -42,3 +43,35 @@ class ProjectTemplateTests(TestCase):
     def test_projects_view_contains_correct_url_to_jira(self):
         self.assertContains(self.response, 'http://jira.west.com/browse/' + self.project.jira_name)
 
+
+class ProjectFormTemplatesTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user_account_normaluser = {
+            'username': 'normalUserName',
+            'password': 'normalUserPassword'
+        }
+        self.user_normaluser = User.objects.create_superuser(
+            username=self.user_account_normaluser['username'],
+            password=self.user_account_normaluser['password'],
+            email=''
+        )
+
+        self.project = Project.objects.create(
+            name='Test Project',
+            jira_name='Test JIRA Name',
+            jira_version='All Versions',
+            score=5
+        )
+
+    def test_project_detail_contains_form(self):
+        self.client.login(
+            username=self.user_account_normaluser['username'],
+            password=self.user_account_normaluser['password']
+        )
+        response = self.client.get(reverse('project_detail',
+                                           args=[str(self.project.id)]),
+                                   follow=True)
+        form = ProjectForm()
+        for field in form:
+            self.assertContains(response, field.html_name)
