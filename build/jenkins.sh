4203dff8f8b8310df8c732e85266e2a1048dc0f7
Songqing Liu <sliu@west.com>
2015-02-19 09:55:32 -0600

    update path of jenkins.txt

--- build/jenkins.sh
+++ build/jenkins.sh
@@ -3,6 +3,6 @@
 virtualenv --no-site-packages --clear env
 source /usr/local/virtualenvs/ceeq/bin/activate
 
-pip install --download-cache /tmp/jenkins/pip-cache -r requirements/jenkins.txt
+pip install --download-cache /tmp/jenkins/pip-cache -r ceeq/requirements/jenkins.txt
 
 python manage.py test --jenkins --settings=ceeq.settings.dev
