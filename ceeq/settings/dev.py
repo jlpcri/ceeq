a96169d8b9e26972f444ad794cc12694198fe10d
Songqing Liu <sliu@west.com>
2015-04-01 10:28:40 -0500

    mistake to edit

--- ceeq/settings/dev.py
+++ ceeq/settings/dev.py
@@ -9,13 +9,13 @@ ALLOWED_HOSTS = [
     'apps.qaci01'
 ]
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'ceeq',
-        'USER': 'visilog',
-        'PASSWORD': '6ewuON0>;wHTe(DttOwjg#5NY)U497xKVwOxmQt60A1%}r:@qC&`7OdSP8u[.l[',
-        'HOST': 'linux6437.wic.west.com',
-        'PORT': '5432'
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'ceeq',
+#         'USER': 'visilog',
+#         'PASSWORD': '6ewuON0>;wHTe(DttOwjg#5NY)U497xKVwOxmQt60A1%}r:@qC&`7OdSP8u[.l[',
+#         'HOST': 'linux6437.wic.west.com',
+#         'PORT': '5432'
+#     }
+# }
