898653d2c349cbfff581f9efe8a3e27786677d32
Songqing Liu <sliu@west.com>
2015-04-01 10:26:13 -0500

    fail connect to linux6437 from qaci01

--- ceeq/settings/qa.py
+++ ceeq/settings/qa.py
@@ -16,13 +16,13 @@ ALLOWED_HOSTS = [
     'linux6438'
 ]
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'ceeq',
-        'USER': 'visilog',
-        'PASSWORD': '6ewuON0>;wHTe(DttOwjg#5NY)U497xKVwOxmQt60A1%}r:@qC&`7OdSP8u[.l[',
-        'HOST': 'linux6437.wic.west.com',
-        'PORT': '5432'
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'ceeq',
+#         'USER': 'visilog',
+#         'PASSWORD': '6ewuON0>;wHTe(DttOwjg#5NY)U497xKVwOxmQt60A1%}r:@qC&`7OdSP8u[.l[',
+#         'HOST': 'linux6437.wic.west.com',
+#         'PORT': '5432'
+#     }
+# }
