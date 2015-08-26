3905f00c92b06b283a81d552058287384af18624
Songqing Liu <sliu@west.com>
2015-08-26 14:35:27 -0500

    Re-generate migration files

--- ceeq/apps/projects/migrations/0004_auto__add_field_project_project_type.py
+++ ceeq/apps/projects/migrations/0004_auto__add_field_project_project_type.py
@@ -8,35 +8,15 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        # Adding model 'ProjectComponent'
-        db.create_table(u'projects_projectcomponent', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('project_type', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['projects.ProjectType'])),
-            ('name', self.gf('django.db.models.fields.CharField')(default='', max_length=50)),
-            ('weight', self.gf('django.db.models.fields.IntegerField')(default=0)),
-        ))
-        db.send_create_signal(u'projects', ['ProjectComponent'])
-
-        # Adding unique constraint on 'ProjectComponent', fields ['project_type', 'name', 'weight']
-        db.create_unique(u'projects_projectcomponent', ['project_type_id', 'name', 'weight'])
-
-        # Adding model 'ProjectType'
-        db.create_table(u'projects_projecttype', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(default='', unique=True, max_length=50)),
-        ))
-        db.send_create_signal(u'projects', ['ProjectType'])
+        # Adding field 'Project.project_type'
+        db.add_column(u'projects_project', 'project_type',
+                      self.gf('django.db.models.fields.related.ForeignKey')(default=1, to=orm['projects.ProjectType']),
+                      keep_default=False)
 
 
     def backwards(self, orm):
-        # Removing unique constraint on 'ProjectComponent', fields ['project_type', 'name', 'weight']
-        db.delete_unique(u'projects_projectcomponent', ['project_type_id', 'name', 'weight'])
-
-        # Deleting model 'ProjectComponent'
-        db.delete_table(u'projects_projectcomponent')
-
-        # Deleting model 'ProjectType'
-        db.delete_table(u'projects_projecttype')
+        # Deleting field 'Project.project_type'
+        db.delete_column(u'projects_project', 'project_type_id')
 
 
     models = {
@@ -63,6 +43,7 @@ class Migration(SchemaMigration):
             'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '200'}),
             'portability': ('django.db.models.fields.IntegerField', [], {'default': '1'}),
+            'project_type': ('django.db.models.fields.related.ForeignKey', [], {'default': '1', 'to': u"orm['projects.ProjectType']"}),
             'reliability': ('django.db.models.fields.IntegerField', [], {'default': '1'}),
             'score': ('django.db.models.fields.DecimalField', [], {'default': '109', 'max_digits': '5', 'decimal_places': '2'}),
             'suitability': ('django.db.models.fields.IntegerField', [], {'default': '1'}),
