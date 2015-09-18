3be41fbc94a98125ac2f91dd41e76e135332fcd4
Songqing Liu <sliu@west.com>
2015-09-18 14:37:02 -0500

    working on calculate internal/overall/uat score/table

--- ceeq/apps/calculator/utils.py
+++ ceeq/apps/calculator/utils.py
@@ -1,1 +1,13 @@
 __author__ = 'sliu'
+
+
+def get_table_data(query_results, uat_type):
+    pass
+
+
+def get_score_data(query_results, uat_type):
+    pass
+
+
+def get_score_by_component(query_results, uat_type):
+    pass
