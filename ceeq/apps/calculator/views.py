d1a52859bc8d2799a4003bdcf81b2fd6eded434f
Songqing Liu <sliu@west.com>
2015-10-16 10:16:58 -0500

    remove user control decorator for celery task

--- ceeq/apps/calculator/views.py
+++ ceeq/apps/calculator/views.py
@@ -1,12 +1,9 @@
-from django.contrib.auth.decorators import user_passes_test
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from ceeq.apps.calculator.tasks import calculate_score
 from ceeq.apps.queries.models import Project
-from ceeq.apps.users.views import user_is_superuser
 
 
-@user_passes_test(user_is_superuser)
 def calculate_score_all(request):
     projects = Project.objects.filter(complete=False)
     for project in projects:
