5a8dd5c4312b92ca454deb053a8f672a8a986316
Songqing Liu <sliu@west.com>
2015-10-20 12:00:55 -0500

    update url

--- ceeq/urls.py
+++ ceeq/urls.py
@@ -14,21 +14,21 @@ v1_api.register(ComponentImpactResource())
 
 urlpatterns = patterns('',
 
-    url(r'^ceeq/$', 'ceeq.apps.core.views.landing', name='landing'),
-    # url(r'^ceeq/', include('ceeq.apps.projects.urls', namespace='projects')),
-    url(r'^ceeq/', include('ceeq.apps.help.urls', namespace='help')),
-    url(r'^ceeq/', include('ceeq.apps.users.urls', namespace='users')),
-    url(r'^ceeq/', include('ceeq.apps.search.urls', namespace='search')),
-    # url(r'^ceeq/', include('ceeq.apps.defects_density.urls', namespace='dds')),
+    url(r'^ceeq_new/$', 'ceeq.apps.core.views.landing', name='landing'),
+    # url(r'^ceeq_new/', include('ceeq.apps.projects.urls', namespace='projects')),
+    url(r'^ceeq_new/', include('ceeq.apps.help.urls', namespace='help')),
+    url(r'^ceeq_new/', include('ceeq.apps.users.urls', namespace='users')),
+    url(r'^ceeq_new/', include('ceeq.apps.search.urls', namespace='search')),
+    # url(r'^ceeq_new/', include('ceeq.apps.defects_density.urls', namespace='dds')),
 
-    url(r'^ceeq/calculator/', include('ceeq.apps.calculator.urls', namespace='calculator')),
-    # url(r'^ceeq/', include('ceeq.apps.formatter.urls', namespace='formatter')),
-    url(r'^ceeq/queries/', include('ceeq.apps.queries.urls', namespace='queries')),
-    url(r'^ceeq/usage/', include('ceeq.apps.usage.urls', namespace='usage')),
+    url(r'^ceeq_new/calculator/', include('ceeq.apps.calculator.urls', namespace='calculator')),
+    # url(r'^ceeq_new/', include('ceeq.apps.formatter.urls', namespace='formatter')),
+    url(r'^ceeq_new/queries/', include('ceeq.apps.queries.urls', namespace='queries')),
+    url(r'^ceeq_new/usage/', include('ceeq.apps.usage.urls', namespace='usage')),
 
 
-    url(r'^ceeq/admin/', include(admin.site.urls)),
-    url(r'^ceeq/api/', include(v1_api.urls)),
+    url(r'^ceeq_new/admin/', include(admin.site.urls)),
+    url(r'^ceeq_new/api/', include(v1_api.urls)),
 )
 
 if settings.DEBUG:
@@ -36,5 +36,5 @@ if settings.DEBUG:
 
     import debug_toolbar
     urlpatterns += patterns('',
-        url(r'^ceeq/__debug__/', include(debug_toolbar.urls)),
+        url(r'^ceeq_new/__debug__/', include(debug_toolbar.urls)),
     )
