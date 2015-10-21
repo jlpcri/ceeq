1b8f278210248515e4fc25a3fbd640773007bb74
Songqing Liu <sliu@west.com>
2015-10-21 14:31:41 -0500

    update package jira from 0.50 to 1.0.3

--- ceeq/settings/local.py
+++ ceeq/settings/local.py
@@ -57,8 +57,8 @@ DATABASES = {
         'USER': 'scorecard',
         'PASSWORD': 'scorecard_development',
         'HOST': 'qaci01.wic.west.com',
-        'PORT': '5432',
-        # 'PORT': '5433'  # another postgres instance
+        # 'PORT': '5432',
+        'PORT': '5433'  # another postgres instance
     }
 }
 
