180ba0e6a58bc0dcfee66f402b8b5373f2879138
Songqing Liu <sliu@west.com>
2015-10-30 09:58:21 -0500

    still get error

--- ceeq/settings/jenkins.py
+++ ceeq/settings/jenkins.py
@@ -2,24 +2,24 @@ from base import *
 
 DEBUG = False
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'TEST_NAME': 'test_database.db'
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'test_database',
-        'USER': 'scorecard',
-        'PASSWORD': 'scorecard_development',
-        'HOST': 'qaci01.wic.west.com',
-        # 'PORT': '5432',
-        'PORT': '5433'  # another postgres instance
+        'ENGINE': 'django.db.backends.sqlite3',
+        'TEST_NAME': 'test_database.db'
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'test_database',
+#         'USER': 'scorecard',
+#         'PASSWORD': 'scorecard_development',
+#         'HOST': 'qaci01.wic.west.com',
+#         # 'PORT': '5432',
+#         'PORT': '5433'  # another postgres instance
+#     }
+# }
 
 INSTALLED_APPS += ('discover_jenkins',)
 
