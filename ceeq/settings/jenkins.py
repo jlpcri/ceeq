ec4097f2ea739c454cb650bb95868f349222d8d7
Songqing Liu <sliu@west.com>
2015-10-30 09:56:10 -0500

    try use postgresql for jenkins

--- ceeq/settings/jenkins.py
+++ ceeq/settings/jenkins.py
@@ -2,10 +2,22 @@ from base import *
 
 DEBUG = False
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'TEST_NAME': 'test_database.db'
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'TEST_NAME': 'test_database.db'
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'test_database',
+        'USER': 'scorecard',
+        'PASSWORD': 'scorecard_development',
+        'HOST': 'qaci01.wic.west.com',
+        # 'PORT': '5432',
+        'PORT': '5433'  # another postgres instance
     }
 }
 
