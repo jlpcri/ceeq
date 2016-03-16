d9fcbcfafbeb327cef10c2d2794ecba6e8985336
Songqing Liu <sliu@west.com>
2016-03-16 13:05:05 -0500

    Re-implement home bar graph with flot

--- ceeq/apps/users/templates/users/home.js
+++ ceeq/apps/users/templates/users/home.js
@@ -206,5 +206,137 @@ $(document).ready(function() {
         })
     });
 
+    $.getJSON("{% url 'queries:fetch_projects_score' %}").done(function(data){
+        //console.log(data);
+        var rawData = [],
+            ticks = [],
+            project_ids = [];
+
+        project_ids = data['id'].reverse();
+        var colors =  ['#7cb5ec', '#434348', '#90ed7d', '#f7a35c', '#8085e9',
+           '#f15c80', '#e4d354', '#8085e8', '#8d4653', '#91e8e1',
+           '#2f7ed8', '#0d233a', '#8bbc21', '#910000', '#1aadce',
+           '#492970', '#f28f43', '#77a1e5', '#c42525', '#a6c96a'
+        ];
+
+        $.each(data['categories'].reverse(), function(k, v){
+            ticks.push([k, v]);
+        });
+        $.each(data['score'].reverse(), function(k, v){
+            rawData.push({
+                data: [[parseFloat(v), k]],
+                color: colors[k]
+            });
+        });
+
+        var dataSet = [{ label: "", data: rawData, color: '' }];
+
+        var options = {
+            series: {
+                bars: {
+                    show: true
+                }
+            },
+            bars: {
+                align: "center",
+                barWidth: 0.5,
+                horizontal: true,
+                fillColor: { colors: [{ opacity: 0.5 }, { opacity: 1}] },
+                lineWidth: 1
+            },
+            xaxis: {
+                axisLabel: "CEEQ Score",
+                axisLabelUseCanvas: true,
+                axisLabelFontSizePixels: 12,
+                axisLabelFontFamily: 'Verdana, Arial',
+                axisLabelPadding: 1,
+                max: 10,
+                tickColor: "#5E5E5E",
+                tickFormatter: function (v, axis) {
+                    return v;
+                },
+                color: "black"
+            },
+            yaxis: {
+                axisLabel: "Project",
+                axisLabelUseCanvas: true,
+                axisLabelFontSizePixels: 12,
+                axisLabelFontFamily: 'Verdana, Arial',
+                axisLabelPadding: 3,
+                tickColor: "#5E5E5E",
+                ticks: ticks,
+                color: "black"
+            },
+            legend: {
+                noColumns: 0,
+                labelBoxBorderColor: "#858585",
+                position: "ne"
+            },
+            grid: {
+                hoverable: true,
+                borderWidth: 2,
+                backgroundColor: { colors: [] },
+                clickable: true
+            }
+        };
+
+        $.plot($('#home_score_flot'), rawData, options);
+        $('#home_score_flot').UseTooltip();
+        $('#home_score_flot').bind('plotclick', function(event, pos, item) {
+            if (item) {
+                var x = item.datapoint[0];
+                var y = item.datapoint[1];
+                location.href = '{{root_path}}' + 'queries/' + project_ids[y];
+            }
+        })
+
+    })
+
 });
 
+var previousPoint = null, previousLabel = null;
+
+$.fn.UseTooltip = function () {
+    $(this).bind("plothover", function (event, pos, item) {
+        if (item) {
+            if ((previousLabel != item.series.label) ||
+            (previousPoint != item.dataIndex)) {
+                previousPoint = item.dataIndex;
+                previousLabel = item.series.label;
+                $("#tooltip").remove();
+
+                var x = item.datapoint[0];
+                var y = item.datapoint[1];
+
+                var color = item.series.color;
+                //alert(color)
+                //console.log(item.series.xaxis.ticks[x].label);
+
+                showTooltip(item.pageX,
+                item.pageY,
+                color,
+                item.series.yaxis.ticks[y].label +
+                " : <strong>" + x + "</strong>");
+            }
+        } else {
+            $("#tooltip").remove();
+            previousPoint = null;
+        }
+    });
+};
+
+function showTooltip(x, y, color, contents) {
+    $('<div id="tooltip">' + contents + '</div>').css({
+        position: 'absolute',
+        display: 'none',
+        top: y - 10,
+        left: x + 10,
+        border: '2px solid ' + color,
+        padding: '3px',
+        'font-size': '9px',
+        'border-radius': '5px',
+        'background-color': '#fff',
+        'font-family': 'Verdana, Arial, Helvetica, Tahoma, sans-serif',
+        opacity: 0.9
+    }).appendTo("body").fadeIn(200);
+}
