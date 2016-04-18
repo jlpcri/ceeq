9d39a5c9ad0dfca3ad089ff5e90937395b7cfb9d
Songqing Liu <sliu@west.com>
2016-04-18 16:58:06 -0500

    jira_version show/hide

--- ceeq/apps/core/templates/core/core.js
+++ ceeq/apps/core/templates/core/core.js
@@ -7,12 +7,16 @@ $('#id_query_field').on('change', function(){
     if (this.value == 'JQL Query'){
         $('label[for="id_jira_key"]').hide();
         $('#id_jira_key').val('JQL');
+        $('label[for="id_jira_version"]').hide();
+        $('#id_jira_version').hide();
         $('#id_jira_key').hide();
         $('label[for="id_query_jql"]').show();
         $('#id_query_jql').show();
     } else {
         $('label[for="id_jira_key"]').show();
         $('#id_jira_key').show();
+        $('label[for="id_jira_version"]').show();
+        $('#id_jira_version').show();
         $('label[for="id_query_jql"]').hide();
         $('#id_query_jql').hide();
     }
