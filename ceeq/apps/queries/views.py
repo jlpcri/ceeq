7e35fb38f3621002a1fbf7038aed42221269ffda
Songqing Liu <sliu@west.com>
2016-05-10 12:11:22 -0500

    Add control of number of projects shown on home bar chart

--- ceeq/apps/queries/views.py
+++ ceeq/apps/queries/views.py
@@ -254,20 +254,30 @@ def fetch_projects_score(request):
     # projects = Project.objects.filter(complete=False).extra(select={'lower_name': 'lower(name)'}).order_by('lower_name')
     projects = sorted(Project.objects.filter(complete=False), key=lambda p: p.jira_data_latest_update, reverse=True)
 
+    try:
+        ls = LiveSettings.objects.get(pk=1)
+        projects_max = ls.home_chart_size
+    except LiveSettings.DoesNotExist:
+        projects_max = 30
+
     data = {}
     data['categories'] = []
     data['score'] = []
     data['id'] = []
+    projects_number = 0
 
     for project in projects:
         score = project.internal_score
-        if score < 10:
-            data['score'].append(str(score))
-        elif score == 103:
+        if score == 103:
             continue
-            # data['score'].append(str(10.00))
         else:
-            data['score'].append(str(0))
+            projects_number += 1
+            if projects_number == projects_max:
+                break
+            if score < 10:
+                data['score'].append(str(score))
+            else:
+                data['score'].append(str(0))
 
         if project.query_field == project.QUERY_VERSION:
             data['categories'].append(project.jira_key.upper() + '-' + project.jira_version)
