dc6a9922f3de957389f6569a787c78133d54d1d0
Songqing Liu <sliu@west.com>
2017-03-16 15:41:52 -0500

    update docker file settings etc.

---
+++ ceeq/settings/docker.py
@@ -0,0 +1,17 @@
+from base import *
+
+DEBUG = True
+
+TEMPLATES[0]['OPTIONS']['debug'] = DEBUG
+
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': '5432'
+    }
+}
